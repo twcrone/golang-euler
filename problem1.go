4c1aefa19513539292d2342d5a0441e575c86c07
Todd Crone <twcrone@gmail.com>
2020-04-21 01:56:16 -0400

    Solve problem1

---
+++ problem1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	
+	var sum = 0
+
+	for i := 1; i < 1000; i++ {
+		f := float64(i)
+		if math.Mod(f, float64(3)) == 0 { 
+			sum += i 
+		} else if math.Mod(f, float64(5)) == 0 { 
+			sum += i 
+		}
+	}
+
+	fmt.Println(sum)
+}
